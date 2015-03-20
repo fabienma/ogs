03c82fab5030d2e490e9dc43c83b03deafb6afde
Thomas Fischer <thomas.fischer@ufz.de>
2015-03-20 14:24:29 +0100

    [WIP] excludeObjectCopy backup - is this really needed?

--- BaseLib/excludeObjectCopy.h
+++ BaseLib/excludeObjectCopy.h
@@ -59,6 +59,34 @@ void excludeObjectCopy(std::vector<T> const& src_vec,
 	dest_vec = excludeObjectCopy(src_vec, exclude_positions);
 }
 
+template <typename T>
+std::vector<T*> excludeObjectCopy(std::vector<T*> const& src_vec,
+	std::vector<std::size_t> const& exclude_positions)
+{
+	std::vector<T*> dest_vec(src_vec.size()-exclude_positions.size());
+
+	assert (exclude_positions.back() < src_vec.size());
+
+	for (std::size_t i(0); i<exclude_positions.front(); ++i) {
+		dest_vec[i] = new T(*(src_vec[i]));
+	}
+
+	std::size_t cnt(exclude_positions.front());
+	for (std::size_t i(1); i<exclude_positions.size(); ++i) {
+		for (std::size_t j(exclude_positions[i-1]+1); j<exclude_positions[i]; j++) {
+			dest_vec[cnt] = new T(*(src_vec[j]));
+			cnt++;
+		}
+	}
+
+	for (std::size_t j(exclude_positions.back()+1); j<src_vec.size(); ++j) {
+		dest_vec[cnt] = new T(*(src_vec[j]));
+		cnt++;
+	}
+
+	return dest_vec;
+}
+
 
 } // end namespace BaseLib
 
